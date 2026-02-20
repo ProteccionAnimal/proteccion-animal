2d524154b66a79458321e2fa5f32c4d4fc5bd090
ProteccionAnimal <censoanimales@lanucia.es>
2026-02-20 13:20:12 +0100

    Segundo Commit
    Añadiendo los mismos archivos pero adaptados a la bd

---
+++ supabase-client.js
@@ -0,0 +1,6 @@
+// supabase-client.js
+const SUPABASE_URL = "https://vkyusosabullpvudxafy.supabase.co";
+const SUPABASE_KEY = "sb_publishable_8JbsqfmoH05omWj1p2jPXg_EwZDntlT";
+
+// Crea cliente global
+window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
